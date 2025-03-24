f289a477f1ec93f18a5784d051a120d3bfcd0852
malek <mezbourmalak@gmail.com>
2025-03-24 23:33:29 +0100

    comment admin recyclerview

--- app/src/main/java/com/example/newstep/Fragments/AdminCommentsFragment.java
+++ app/src/main/java/com/example/newstep/Fragments/AdminCommentsFragment.java
@@ -1,23 +1,75 @@
 package com.example.newstep.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.newstep.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.newstep.Adapters.ReportCommentAdapter;
+import com.example.newstep.Models.ReportComment;
+import com.example.newstep.R;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class AdminCommentsFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_admin_comments, container, false);
+    private RecyclerView recyclerViewReports;
+    private ReportCommentAdapter adapter;
+    private FirebaseFirestore firestore;
 
-        return rootView;
+    public AdminCommentsFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_admin_comments, container, false);
+
+        recyclerViewReports = view.findViewById(R.id.recyclerViewReports);
+        recyclerViewReports.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        firestore = FirebaseFirestore.getInstance();
+
+
+        loadReports();
+
+        return view;
+    }
+
+    private void loadReports() {
+        Query query = firestore.collection("ReportComments")
+                .orderBy("lastReportTime", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<ReportComment> options =
+                new FirestoreRecyclerOptions.Builder<ReportComment>()
+                        .setQuery(query, ReportComment.class)
+                        .build();
+
+        adapter = new ReportCommentAdapter(options, getContext());
+        recyclerViewReports.setAdapter(adapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (adapter != null) {
+            adapter.startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (adapter != null) {
+            adapter.stopListening();
+        }
     }
 }
