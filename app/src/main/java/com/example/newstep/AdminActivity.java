b6e4eb1808d468819ff819d78335000e5d8fab8d
selma <farfarselma9@gmail.com>
2025-03-25 16:49:06 +0100

    Some Change in AdminActivity désigne

--- app/src/main/java/com/example/newstep/AdminActivity.java
+++ app/src/main/java/com/example/newstep/AdminActivity.java
@@ -1,7 +1,11 @@
 package com.example.newstep;
 
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,12 +23,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AdminActivity extends AppCompatActivity {
     BottomNavigationView bottomView;
+    TextView textView;
     ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_admin);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -38,6 +43,8 @@ public class AdminActivity extends AppCompatActivity {
                     .commit();
         }
         bottomView=findViewById(R.id.bottomMenu);
+        textView=findViewById(R.id.title_textview);
+        applyGradientToText(textView);
         bottomView.setOnItemSelectedListener(item -> {
             if(item.getItemId()==R.id.nav_comments){
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new AdminCommentsFragment()).commit();
@@ -50,5 +57,16 @@ public class AdminActivity extends AppCompatActivity {
             });
             return true;
         });
+    }
+
+    private void applyGradientToText(TextView textView) {
+        TextPaint paint = textView.getPaint();
+        float width = paint.measureText(textView.getText().toString());
+
+        Shader textShader = new LinearGradient(0, 0, width, textView.getTextSize(),
+                new int[]{0xFFFFFFFF, 0xFFFF69B4, 0xFF800080},
+                null, Shader.TileMode.CLAMP);
+
+        textView.getPaint().setShader(textShader);
     }
 }
