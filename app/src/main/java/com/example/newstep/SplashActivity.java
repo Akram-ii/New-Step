cee43f1cf69d8f9f7ff984d7b98a919d5e6819db
Akram-ii <154929704+Akram-ii@users.noreply.github.com>
2025-04-16 18:45:30 +0200

    navigation for first time in the app +rules

---
+++ app/src/main/java/com/example/newstep/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.newstep;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+        getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences("AppPrefs", Context.MODE_PRIVATE);
+                Boolean firstTime = sharedPreferences.getBoolean("firstTime", true);
+                if(!firstTime){
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();}
+                else{
+                startActivity(new Intent(SplashActivity.this, WelcomeActivity.class) );
+                }
+            }
+        },1000);
+
+    }}
+
