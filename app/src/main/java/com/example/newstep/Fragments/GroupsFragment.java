093ae10b7189e11e6330de262b1fa62c709221fd
selma <farfarselma9@gmail.com>
2025-04-18 19:50:21 +0100

    filter_group

--- app/src/main/java/com/example/newstep/Fragments/GroupsFragment.java
+++ app/src/main/java/com/example/newstep/Fragments/GroupsFragment.java
@@ -3,7 +3,9 @@ package com.example.newstep.Fragments;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -48,6 +51,10 @@ import com.example.newstep.Models.ChatroomModel;
 import com.example.newstep.R;
 import com.example.newstep.Util.FirebaseUtil;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -57,6 +64,8 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -69,6 +78,7 @@ public class GroupsFragment extends Fragment {
     private AllGroupsAdapter adapter;
     private FirebaseFirestore db;
     private FirebaseAuth auth;
+    private ImageView filter_groups;
 
     @Nullable
     @Override
@@ -78,12 +88,19 @@ public class GroupsFragment extends Fragment {
         recyclerView = rootView.findViewById(R.id.groupsRecycler);
         addBtn = rootView.findViewById(R.id.addBtn);
         chat = rootView.findViewById(R.id.chatsBtn);
+        filter_groups=rootView.findViewById(R.id.filter_group);
 
         db = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
 
         addBtn.setOnClickListener(v -> showCreateGroupPopup(v));
         chat.setOnClickListener(v -> setupChatBtn());
+        filter_groups.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFilterGroupsPopup();
+            }
+        });
 
         setupRecycler();
 
@@ -204,6 +221,7 @@ public class GroupsFragment extends Fragment {
         groupData.put("lastMsgSenderId",FirebaseUtil.getCurrentUserId());
         groupData.put("lastMsgSent", "");
         groupData.put("lastMsgTimeStamp", null);
+        groupData.put("creationTimestamp", Timestamp.now());
         groupData.put("unseenMsg", 0);
 
         newGroupRef.set(groupData)
@@ -231,4 +249,187 @@ public class GroupsFragment extends Fragment {
         transaction.addToBackStack(null);
         transaction.commitAllowingStateLoss();
     }
+
+    private void showFilterGroupsPopup() {
+        if (getActivity() == null) return;
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View popUpView = inflater.inflate(R.layout.popup_filter_groups, null);
+
+        EditText searchGroupName = popUpView.findViewById(R.id.searchGroupName);
+        ChipGroup privacyChipGroup = popUpView.findViewById(R.id.privacyChipGroup);
+        MaterialButton filter = popUpView.findViewById(R.id.btnFilterNow);
+        ImageView back= popUpView.findViewById(R.id.back_popup);
+
+        final PopupWindow popupWindow = new PopupWindow(popUpView,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true);
+
+
+        WindowManager.LayoutParams layoutParams = requireActivity().getWindow().getAttributes();
+        layoutParams.alpha = 0.5f;
+        requireActivity().getWindow().setAttributes(layoutParams);
+
+        popupWindow.showAtLocation(getActivity().findViewById(android.R.id.content), Gravity.CENTER, 0, 0);
+
+        popupWindow.setOnDismissListener(() -> {
+            WindowManager.LayoutParams originalParams = requireActivity().getWindow().getAttributes();
+            originalParams.alpha = 1.0f;
+            requireActivity().getWindow().setAttributes(originalParams);
+        });
+
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String searchName = searchGroupName.getText().toString().trim();
+                List<String> selectedFilters = getSelectedPrivacy(privacyChipGroup);
+                applyFilters(searchName, selectedFilters);
+                popupWindow.dismiss();
+            }
+        });
+        searchGroupName.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                Drawable[] drawables = searchGroupName.getCompoundDrawables();
+                Drawable drawableStart = drawables[0]; // drawableStart = left
+
+                if (drawableStart != null) {
+                    int drawableWidth = drawableStart.getBounds().width();
+
+
+                    if (event.getX() <= (drawableWidth + 40)) {
+                        searchGroupName.setText("");
+                        return true;
+                    }
+                }
+            }
+            return false;
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupWindow.dismiss();
+            }
+        });
+
+
+    }
+
+
+
+
+
+    private List<String> getSelectedPrivacy(ChipGroup chipGroup) {
+        List<String> selected = new ArrayList<>();
+        for (int i = 0; i < chipGroup.getChildCount(); i++) {
+            Chip chip = (Chip) chipGroup.getChildAt(i);
+            if (chip.isChecked()) {
+                selected.add(chip.getText().toString());
+            }
+        }
+        return selected;
+    }
+    private void applyFilters(String searchName, List<String> selectedFilters) {
+        Query query = FirebaseUtil.allChatroomCollectionRef()
+                .whereEqualTo("isGroup", 1);
+
+        if (!TextUtils.isEmpty(searchName)) {
+            query = query.whereGreaterThanOrEqualTo("groupName", searchName)
+                    .whereLessThanOrEqualTo("groupName", searchName + "\uf8ff");
+        }
+
+
+
+        if (selectedFilters.contains("Public")) {
+            query = query.whereEqualTo("privacy", "Public");
+        } else if (selectedFilters.contains("Private")) {
+            query = query.whereEqualTo("privacy", "Private");
+        }
+
+        if (selectedFilters.contains("Popular")) {
+            query = query.whereGreaterThan("number_members", 5);
+        }
+        if (selectedFilters.contains("Very Popular")) {
+            query = query.whereGreaterThan("number_members", 30);
+        }
+        if (selectedFilters.contains("Most Popular")) {
+            query = query.whereGreaterThan("number_members", 50);
+        }
+        if (selectedFilters.contains("Active Today")) {
+
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.MILLISECOND, 0);
+            long startOfDay = calendar.getTimeInMillis();
+
+
+            Timestamp timestampStartOfDay = new Timestamp(new Date(startOfDay));
+
+
+            Calendar endOfDayCalendar = Calendar.getInstance();
+            endOfDayCalendar.set(Calendar.HOUR_OF_DAY, 23);
+            endOfDayCalendar.set(Calendar.MINUTE, 59);
+            endOfDayCalendar.set(Calendar.SECOND, 59);
+            endOfDayCalendar.set(Calendar.MILLISECOND, 999);
+            long endOfDay = endOfDayCalendar.getTimeInMillis();
+
+
+            Timestamp timestampEndOfDay = new Timestamp(new Date(endOfDay));
+
+
+            query = query.whereGreaterThanOrEqualTo("lastMsgTimeStamp", timestampStartOfDay)
+                    .whereLessThanOrEqualTo("lastMsgTimeStamp", timestampEndOfDay);
+        }
+        if (selectedFilters.contains("Newest")) {
+            query = query.orderBy("creationTimestamp", Query.Direction.DESCENDING);
+        } else if (selectedFilters.contains("Oldest")) {
+            query = query.orderBy("creationTimestamp", Query.Direction.ASCENDING);
+        }
+
+        if (selectedFilters.contains("My Groups")) {
+            query = query.whereEqualTo("ownerId", FirebaseUtil.getCurrentUserId());
+        }
+
+
+
+            FirestoreRecyclerOptions<ChatroomModel> options =
+                new FirestoreRecyclerOptions.Builder<ChatroomModel>()
+                        .setQuery(query, ChatroomModel.class)
+                        .setLifecycleOwner(this)
+                        .build();
+
+        adapter.stopListening();
+        adapter.updateOptions(options);
+        recyclerView.scrollToPosition(0);
+        adapter.notifyDataSetChanged();
+        adapter.startListening();
+    }
+
+
+
+
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+
+
+
+
 }
